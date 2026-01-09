ba3eb42e008a4c9330add0dcda5d62b982cfbed9
RicardojCh10 <ricardojch.12@gmail.com>
2026-01-09 15:59:45 -0500

    Debug de Asset Controller

--- app/Http/Controllers/AssetController.php
+++ app/Http/Controllers/AssetController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use App\Services\AssetService;
-use App\Services\AssignmentPdfService;
+// use App\Services\AssignmentPdfService;
 use App\Http\Requests\StoreAssetRequest;
 use App\Http\Requests\UpdateAssetRequest;
 use App\Http\Resources\AssetResource;
@@ -17,9 +17,11 @@ class AssetController extends Controller
 
     // Inyección de Dependencia del Servicio
     public function __construct(AssetService $assetService, AssignmentPdfService $assignmentPdfService)
+        // public function __construct(AssetService $assetService, AssignmentPdfService $assignmentPdfService)
+
     {
         $this->assetService = $assetService;
-        $this->assignmentPdfService = $assignmentPdfService;
+        // $this->assignmentPdfService = $assignmentPdfService;
     }
 
     // Listar Activos con filtro por propiedad
@@ -49,26 +51,26 @@ class AssetController extends Controller
         return AssetResource::collection($assets);
     }
 
-    public function downloadAssignment($id)
-    {
-        // Buscamos el activo y cargamos la relación con el empleado
-        $asset = Asset::with(['assigned_to', 'location'])->findOrFail($id);
+    // public function downloadAssignment($id)
+    // {
+    //     // Buscamos el activo y cargamos la relación con el empleado
+    //     $asset = Asset::with(['assigned_to', 'location'])->findOrFail($id);
 
-        if (!$asset->member_id) {
-            return response()->json(['message' => 'Este activo no está asignado a nadie.'], 400);
-        }
+    //     if (!$asset->member_id) {
+    //         return response()->json(['message' => 'Este activo no está asignado a nadie.'], 400);
+    //     }
 
-        // Generamos el binario del PDF
-        $pdfContent = $this->assignmentPdfService->generatePdf($asset);
+    //     // Generamos el binario del PDF
+    //     $pdfContent = $this->assignmentPdfService->generatePdf($asset);
 
-        // Preparamos el nombre del archivo (Sanitizado)
-        $filename = 'Acta_' . preg_replace('/[^A-Za-z0-9\-]/', '', $asset->serial_number) . '.pdf';
+    //     // Preparamos el nombre del archivo (Sanitizado)
+    //     $filename = 'Acta_' . preg_replace('/[^A-Za-z0-9\-]/', '', $asset->serial_number) . '.pdf';
 
-        return response($pdfContent, 200, [
-            'Content-Type' => 'application/pdf',
-            'Content-Disposition' => 'inline; filename="' . $filename . '"',
-        ]);
-    }
+    //     return response($pdfContent, 200, [
+    //         'Content-Type' => 'application/pdf',
+    //         'Content-Disposition' => 'inline; filename="' . $filename . '"',
+    //     ]);
+    // }
 
     // Crear un nuevo Activo
       /**
