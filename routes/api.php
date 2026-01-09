4015cc4d0e783f30ee98ece8635c1d672cfde4d9
RicardojCh10 <ricardojch.12@gmail.com>
2026-01-09 16:28:59 -0500

    Se agrega la ruta de Assets con el get personalizado de download PDF

--- routes/api.php
+++ routes/api.php
@@ -9,31 +9,29 @@ use App\Http\Controllers\PropertyController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\MaintenanceLogController;
 
- // Rutas Publicas de Autenticación
-  Route::post('auth/login', [AuthController::class, 'login']);
+// Rutas Publicas
+Route::post('auth/login', [AuthController::class, 'login']);
 
-  // Rutas Protegidas de Autenticación
-  Route::group(['middleware' => ['auth:api']], function () {
+// Rutas Protegidas (Todo lo de adentro requiere Token)
+Route::group(['middleware' => ['auth:api']], function () {
 
-    //Auth
-      Route::post('auth/logout', [AuthController::class, 'logout']);
-      Route::post('auth/refresh', [AuthController::class, 'refresh']);
-      Route::get('auth/me', [AuthController::class, 'me']);
+    // Auth
+    Route::post('auth/logout', [AuthController::class, 'logout']);
+    Route::post('auth/refresh', [AuthController::class, 'refresh']);
+    Route::get('auth/me', [AuthController::class, 'me']);
 
-      // Rutas Protegidas de Recursos
-      Route::middleware('auth:api')->group(function () {
-      // Recursos API
+    // --- RECURSOS API ---
 
-      //Rutas de Users
-      Route::apiResource('users', UserController::class);
-      //Rutas de assets
-      Route::apiResource('assets', AssetController::class);
-      //Rutas de Property
-      Route::apiResource('properties', PropertyController::class);
-      //Rutas de Members
-      Route::apiResource('members', MemberController::class);
-      //Rutas de Maintenance Logs
-      Route::apiResource('maintenance-logs', MaintenanceLogController::class);
-      });
+    // Users
+    Route::apiResource('users', UserController::class);
 
-  });
+    // Assets 
+    Route::get('/assets/{id}/download-assignment', [AssetController::class, 'downloadAssignment']);
+    Route::apiResource('assets', AssetController::class);
+
+    // Otros recursos
+    Route::apiResource('properties', PropertyController::class);
+    Route::apiResource('members', MemberController::class);
+    Route::apiResource('maintenance-logs', MaintenanceLogController::class);
+
+});
