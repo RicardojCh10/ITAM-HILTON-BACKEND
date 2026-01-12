4fd8372f6ebe1b02f9c63d15bebc5af9da3808e5
RicardojCh10 <ricardojch.12@gmail.com>
2026-01-12 15:10:01 -0500

    fix(apache): resolve multiple MPM loaded error by enforcing mpm_prefork

--- start.sh
+++ start.sh
@@ -4,21 +4,27 @@ set -e
 
 echo "🚀 Iniciando contenedor (Apache)..."
 
-# 1. Configurar Puerto de Apache dinámicamente
+# 1. SOLUCIÓN AL ERROR MPM (El Desempate)
+echo "🔧 Ajustando motores MPM de Apache..."
+a2dismod mpm_event || true
+a2dismod mpm_worker || true
+a2enmod mpm_prefork || true
+
+# 2. Configurar Puerto dinámicamente
 echo "🔌 Configurando puerto $PORT..."
 sed -i "s/Listen 80/Listen ${PORT:-8080}/g" /etc/apache2/ports.conf
 sed -i "s/:80/:${PORT:-8080}/g" /etc/apache2/sites-enabled/000-default.conf
 
-# 2. Configuración de Laravel
-echo "🧹 Limpiando caché (Modo Seguro)..."
+# 3. Limpieza de Caché
+echo "🧹 Limpiando caché..."
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
 
-# 3. Migraciones
+# 4. Migraciones
 echo "📦 Migrando base de datos..."
 php artisan migrate --force
 
-# 4. Iniciar Apache
+# 5. Iniciar Apache
 echo "🔥 Arrancando Apache en primer plano..."
 exec apache2-foreground
